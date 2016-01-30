35d208075dbd3ca54e713167a8707c34e5ff8a3a
Henrik Phan <henrik.p95@gmail.com>
2016-01-30 13:29:42 +0100

    Player movement, walls and collision
    programmer graphics represent

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float WalkSpeed { get; set; }
+    public float RunSpeed { get; set; }
+
+    float Speed;
+
+    BoxCollider2D boxCollider;
+    new Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        Speed = 500f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Movement();
+        if(Input.GetKey)
+	}
+
+    void Movement()
+    {
+        float horizontal = HorizontalMovement();
+        float vertical = VerticalMovement();
+        Vector3 movement = new Vector3(horizontal, vertical, 0);
+        rigidbody2D.velocity = movement;
+        if (Input.GetKey("z"))
+            Examine();
+    }
+
+    float HorizontalMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal > 0)
+            horizontal = 1;
+        else if (horizontal < 0)
+            horizontal = -1;
+        horizontal *= Speed * Time.deltaTime;
+        return horizontal;
+    }
+
+    float VerticalMovement()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        if (vertical > 0)
+            vertical = 1;
+        else if (vertical < 0)
+            vertical = -1;
+        vertical *= Speed * Time.deltaTime;
+        return vertical;
+    }
+
+    void Examine()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/RPGText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RPGText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartText();
+	}
+	
+    public void StartText()
+    {
+        Debug.Log("test");
+        StartCoroutine(TextScroll());
+    }
+
+    public IEnumerator TextScroll()
+    {
+        Debug.Log("scroll");
+        Text text = GetComponent<Text>();
+        string dialogue = text.text;
+        for (int i = 0; i < dialogue.Length; i++)
+        {
+            text.text = dialogue.Substring(0, i);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
