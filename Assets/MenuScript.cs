7fb5454fb3e7b961d6a0fd54d50ed2d8ac3631a5
Henrik Phan <henrik.p95@gmail.com>
2016-01-31 13:04:22 +0100

    Main menu
    camera is weird

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public GameObject MainMenu;
+    public GameObject HelpMenu;
+
+	public void StartGame()
+    {
+        Application.LoadLevel("main");
+    }
+
+    public void ShowHelp()
+    {
+        MainMenu.SetActive(false);
+        HelpMenu.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMenu()
+    {
+        MainMenu.SetActive(true);
+        HelpMenu.SetActive(false);
+    }
+}
