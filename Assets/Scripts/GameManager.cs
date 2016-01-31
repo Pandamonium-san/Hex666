13a28cf18dabba0ba0559df72f95917f6fe3158f
Henrik Phan <henrik.p95@gmail.com>
2016-01-31 09:46:25 +0100

    Wald + Nox animations, fixed dialogue box

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,8 @@ public class GameManager : MonoBehaviour
     public Transform dialogue;
     public Text textBox;
     public List<GameObject> avatars;
-    public Vector3 posLeft, posRight;
+    //public Vector3 posLeft, posRight;
+    public float yPos;
 
     List<GameObject> instAvatars;
     Queue<string> msgQueue;
@@ -24,8 +25,8 @@ public class GameManager : MonoBehaviour
         {
             GameObject newAv = Instantiate(av);
             instAvatars.Add(newAv);
-            newAv.transform.parent = canvas.transform;
-            //newAv.SetActive(false);
+            newAv.transform.parent = dialogue.transform;
+            newAv.SetActive(false);
         }
     }
 
@@ -36,20 +37,20 @@ public class GameManager : MonoBehaviour
             PlayMessage("hello this is the end of the beginning have you tried my new however what if you didn't but yes in the home");
         if (Input.GetKeyDown("2"))
         {
-            ShowAvatar(0, true);
+            ShowAvatar(0, 500);
             curLeft.GetComponent<Animator>().SetTrigger("Happy");
         }
         if (Input.GetKeyDown("3"))
         {
             curLeft.GetComponent<Animator>().SetTrigger("Angry");
         }
-        if(Input.GetKeyDown("4"))
+        if (Input.GetKeyDown("4"))
         {
             curLeft.GetComponent<Animator>().SetTrigger("Idle");
         }
         if (Input.GetKeyDown("5"))
         {
-            ShowAvatar(2, false);
+            ShowAvatar(2, -500);
         }
         if (Input.GetKeyDown("6"))
         {
@@ -57,6 +58,9 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Shows a single message.
+    /// </summary>
     public void PlayMessage(string message)
     {
         dialogue.gameObject.SetActive(true);
@@ -71,6 +75,9 @@ public class GameManager : MonoBehaviour
         txScr.StartTextScroll();
     }
 
+    /// <summary>
+    /// Creates a continuous dialoguebox.
+    /// </summary>
     public void PlayMessage(Queue<string> messages)
     {
         dialogue.gameObject.SetActive(true);
@@ -90,27 +97,17 @@ public class GameManager : MonoBehaviour
         PlayMessage(msgQueue.Dequeue());
     }
 
-    public void ShowAvatar(int index, bool left)
+    /// <summary>
+    /// Shows avatar above dialogue box.
+    /// </summary>
+    /// <param name="index">0 = Myyn, 1 = Nox, 2 = Wald (set in game manager)</param>
+    /// <param name="xPos">X-position of avatar</param>
+    public void ShowAvatar(int index, float xPos)
     {
         Debug.Log("showing" + index);
         GameObject av = instAvatars[index];
-        if (left)
-        {
-            if (curLeft)
-                curLeft.SetActive(false);
-            av.transform.localPosition = posLeft;
-            curLeft = av;
-            //Mirror sprite
-            //Vector3 scale = av.transform.localScale;
-            //scale = new Vector3(scale.x * -1, scale.y, scale.z);
-        }
-        else
-        {
-            if (curRight)
-                curRight.SetActive(false);
-            av.transform.localPosition = posRight;
-            curRight = av;
-        }
+        av.transform.localPosition = new Vector2(xPos, yPos);
+        curLeft = av;
         av.SetActive(true);
     }
 
