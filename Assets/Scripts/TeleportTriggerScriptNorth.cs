89a5cbb93bbf65d4df7e68da4876252cbcb43cff
FredrikLH <ad0935@WCCZC5192880.ad.mah.se>
2016-01-31 05:48:13 +0100

    started work on sceen teleport

---
+++ Assets/Scripts/TeleportTriggerScriptNorth.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportTriggerScriptNorth : EventTrigger
+{
+    public override void Trigger(Player player)
+    {
+        Vector3 OldPos = player.transform.position;
+        OldPos.y += 20;
+        player.transform.position = OldPos;
+	}
+}
